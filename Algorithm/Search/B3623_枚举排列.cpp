f468e42123f1ed90246d1d209ed3825e3a3e1d51
Lmyasdf <1901187412@qq.com>
2026-02-28 18:14:28 +0800

    feat：枚举序列，DFS解决

--- Algorithm/Search/B3623_枚举排列.cpp
+++ Algorithm/Search/B3623_枚举排列.cpp
@@ -1,5 +1,32 @@
+/* 
+    题目：枚举序列
+    链接：https://www.luogu.com.cn/problem/B3623
+ */
+
 #include<iostream>
+#include<vector>
 using namespace std;
+const int N = 15;
+int st[N];
+vector<int>v;
+int n,k;
+void dfs(){
+    if(v.size()==k){
+        for(int a:v)cout<<a<<" ";
+        cout<<endl;
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        if(!st[i]){
+            st[i] = 1;//标记这个数是否选择过
+            v.push_back(i);
+            dfs();
+            v.pop_back();
+            st[i] = 0;
+        }
+    }
+}
 int main(){
-    
+    cin>>n>>k;
+    dfs();
 }
