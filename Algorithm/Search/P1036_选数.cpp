e80b96f1384f6afa2d1efef6c948d63a393531b8
Lmyasdf <1901187412@qq.com>
2026-03-03 10:18:55 +0800

    perf: 将素数判断从逐个该问线性筛预处理

--- Algorithm/Search/P1036_选数.cpp
+++ Algorithm/Search/P1036_选数.cpp
@@ -6,24 +6,44 @@
     2.判断过程在dfs出口处就可以了，不用再创建数组存这些结果最后在main中遍历判断
 */
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<cstring>
 using namespace std;
-int sum = 0,cnt = 0;
+const int N = 1e8;
+int cnt = 0;
 int n,k;
+
+bool isPrime[N];
+vector<int>primes;
 vector<int>v,v2;
-bool isPrime(long long x){
-    if(x<2)return false;
-    if(x==2||x==3)return true;
-    if(x%2==0||x%3==0)return false;
-    for(long long i=5;i*i<=x;i+=6)if(x%i==0||x%(i+2)==0)return false;
-    return true;
+
+// bool isPrime(long long x){
+//     if(x<2)return false;
+//     if(x==2||x==3)return true;
+//     if(x%2==0||x%3==0)return false;
+//     for(long long i=5;i*i<=x;i+=6)if(x%i==0||x%(i+2)==0)return false;
+//     return true;
+// }
+
+//线性筛：
+void linearSieve(){
+    memset(isPrime,true,sizeof(isPrime));//先全初始化为true
+    isPrime[0] = false;//两个特殊值
+    isPrime[1] = false;
+    for(int i=2;i<=N;i++){//遍历2~N
+        if(isPrime[i])primes.push_back(i);//是素数就入素数组
+        for(int a:primes){//遍历素数数组
+            if(1LL*a*i>N)break;
+            isPrime[a*i] = false;//标记合数
+            if(i%a==0)break;//更大的素数*i得到的数后面一定会被后面i更大*a标记
+        }
+    }
 }
 void dfs(int begin){
     if(v2.size()==k){
+        int sum=0;
        for(int i=0;i<k;i++)sum+=v2[i];
-       if(isPrime(sum))cnt++;
-       sum=0;
+       if(isPrime[sum])cnt++;
        return;
     }
     for(int i=begin;i<n;i++){
@@ -32,14 +52,14 @@ void dfs(int begin){
         v2.pop_back();
     }
 }
-int main(){    
+int main(){
+    linearSieve();    
     cin>>n>>k;
     for(int i=0;i<n;i++){
         int a;
         cin>>a;
         v.push_back(a);
     }
-    sort(v.begin(),v.end());
     dfs(0);
     cout<<cnt;
 }
