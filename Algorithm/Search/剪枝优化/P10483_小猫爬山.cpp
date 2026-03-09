6dbee514edaabed0adc882a21d8928d2449d08bb
Lmyasdf <1901187412@qq.com>
2026-03-09 09:48:53 +0800

    refactor：重写注释逻辑

--- Algorithm/Search/剪枝优化/P10483_小猫爬山.cpp
+++ Algorithm/Search/剪枝优化/P10483_小猫爬山.cpp
@@ -18,19 +18,21 @@ bool mycmp(int a,int b){
     return a>b;
 }
 void dfs(int pos,int car){
+    //策略二：最优性剪枝
     if(car >= ret)return;//大于当前的解，必然不是最优解，直接return
     if(pos==n){//递归出口
         ret = min(ret,car);
         return;
     }
-
+    //策略三：优化搜索顺序
+    //先安排在已有车辆上
     for(int i=1;i<=car;i++){//遍历所有车
         if(c[i]+v[pos]>M)continue;//放不下就continue
         c[i] += v[pos];
         dfs(pos+1,car);
         c[i] -= v[pos];
     }
-    //尝试加一辆车
+    //重加一辆车
     c[car+1] += v[pos];
     dfs(pos+1,car+1);
     c[car+1] -= v[pos];        
@@ -44,6 +46,7 @@ int main(){
         cin>>a;
         v.push_back(a);
     }
+    //策略三：优化搜索顺序
     sort(v.begin(),v.end(),mycmp);//自定义降序排列，先放大的进缆车，快速填满缆车
     dfs(0,1);
     cout<<ret;
