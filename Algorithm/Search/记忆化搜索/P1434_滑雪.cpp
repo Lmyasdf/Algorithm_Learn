3a141ce8de13b707be0b9f66f1c428bb4acd3552
Lmyasdf <1901187412@qq.com>
2026-03-14 19:58:38 +0800

    feat：记忆化搜索解决化学问题

--- Algorithm/Search/记忆化搜索/P1434_滑雪.cpp
+++ Algorithm/Search/记忆化搜索/P1434_滑雪.cpp
@@ -1,4 +1,10 @@
+/* 题目：滑雪
+https://www.luogu.com.cn/problem/P1434
+思路：遍历所有点，从每个点开始dfs，最后输出最大值，dfs中应用记忆化搜索，16行是关键
+没搜索过，就给他赋值表示滑过的同时也赋了该点可以滑最少本身一个位置
+ */
 #include<iostream>
+#include<cstring>
 using namespace std;
 const int N = 110;
 int f[N][N],a[N][N];
@@ -7,13 +13,13 @@ int ret = 0;
 int dx[] = {-1,1,0,0};
 int dy[] = {0,0,-1,1};
 int dfs(int x,int y){
-    int max =1;
+    if(f[x][y])return f[x][y];
+    f[x][y] = 1;//关键
     for(int i=0;i<4;i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
         if(nx>=1&&ny>=1&&nx<=n&&ny<=m&&a[nx][ny]>a[x][y]){
-            max++;
-            f[nx][ny] = dfs(nx,ny);
+            f[x][y] = max(dfs(nx,ny)+1,f[x][y]);
         }
     }
     return f[x][y];
