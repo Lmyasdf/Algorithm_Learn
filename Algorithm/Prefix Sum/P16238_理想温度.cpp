6dc1993ff6c647dd9d47877530dbe8b9d9826f52
Lmyasdf <1901187412@qq.com>
2026-05-25 19:52:35 +0800

    refactor：丰富代码思路注释

--- Algorithm/Prefix Sum/P16238_理想温度.cpp
+++ Algorithm/Prefix Sum/P16238_理想温度.cpp
@@ -5,7 +5,7 @@
 
 2.发现操作等价于：区间内选一个 k 达成达标，区间外保留原本达标的。
 
-*3.推出目标：总达标数 = 原本达标数 + (区间内满足 d[i]==k 的数量 ? 区间内原本达标数量)。
+*3.推出目标：总达标数 = 原本达标数 + (区间内满足 d[i]==k 的数量 - 区间内原本达标数量)。
 
 4.于是问题变成在 +1/-1/0 序列上找最大子段和。
 
@@ -31,21 +31,24 @@ int main(){
         if(d[i])m[d[i]].push_back(i);
         zero_cnt += (d[i]==0);
     }
+    //构建统计0数量的前缀和数组
     for(int i=1;i<=n;i++){
         pre[i] = pre[i-1] + (d[i]==0);//统计0（已达到理想）数量的前缀和
     }
     int maxret = zero_cnt;//初始化最大值为0总数目，即不调整时候的达理想数
     //核心：
-    for(auto &x : m){
-        auto &p = x.second;
+    for(auto &x : m){//遍历非0的d值
+        auto &p = x.second;//对应索引数组
+        //最大子段和模板
         int cur = 1;
         int best = 1;
         for(int i=1;i<p.size();i++){
+            //区间[L,R]中0计算方法：pre[R] - pre[L-1]
             int zero = pre[p[i]-1] - pre[p[i-1]];//注意这个区间，不去包含端点
             cur = max(1,cur+1-zero);//重置 or 累加(+区间端点，-区间内0个数)
             best = max(best,cur);//更新最优解
         }
-        maxret = max(maxret,best+zero_cnt);
+        maxret = max(maxret,best+zero_cnt);//结果 = 总d零数 + (调整后理想数 - 影响的d零数) 
     }
 
     cout<<maxret;
