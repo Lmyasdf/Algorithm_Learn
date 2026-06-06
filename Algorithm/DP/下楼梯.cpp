018b5742978951d0262f6a7e95a33b70172c8f3e
Lmyasdf <1901187412@qq.com>
2026-06-06 22:53:21 +0800

    feat:解决动态规划入门题，了解并总结动态规划常规解题步骤

---
+++ Algorithm/DP/下楼梯.cpp
@@ -0,0 +1,24 @@
+//https://www.luogu.com.cn/problem/P10250
+#include<iostream>
+using namespace std;
+const int N = 65;
+//1.确定数组存啥状态（定义状态表示），此题表示为到达n级所需步数
+long long f[N];
+long long fun(int n){
+    if(n<3)return f[n];
+    //2.推导状态转移方程，最多走3级，故f[n]就是前三级所需步数的累加
+    //4.确定调表顺序，本题从前往后
+    //5.确定最终结果
+    if(f[n-1]!=0)return f[n] = f[n-1] + f[n-2] + f[n-3];
+    return f[n] = fun(n-1) + fun(n-2) + fun(n-3);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    //3.初始化，显而易见的位置填上值
+    f[0] = 1;
+    f[1] = 1;
+    f[2] = 2;
+    cout<<fun(n)<<endl;
+}
