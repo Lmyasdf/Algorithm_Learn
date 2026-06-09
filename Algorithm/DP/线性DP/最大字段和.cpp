374cd4136b5247782ab9518761e81cead7f7c0ec
Lmyasdf <1901187412@qq.com>
2026-06-09 22:59:05 +0800

    refactor:优化代码

--- Algorithm/DP/线性DP/最大字段和.cpp
+++ Algorithm/DP/线性DP/最大字段和.cpp
@@ -6,12 +6,10 @@ int f[N],a[N];//1.确定状态：以该位置为终点，从开始到这里的最大和
 int main(){
     int n;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>a[i];
-    }
     //3.初始化
-    int ret = a[1];//错误：初始化为0，在全为负数的测试点会挂掉
-    for(int i=1;i<=n;i++){
+    int ret = -0x3f3f3f3f;//错误：初始化为0，在全为负数的测试点会挂掉
+    for(int i=1;i<=n;i++){//数组a存数可以和f更新同步进行
+        cin>>a[i];
         f[i] = max(f[i-1]+a[i],a[i]);//2.推导状态转移方程
         ret = max(f[i],ret);
     }
