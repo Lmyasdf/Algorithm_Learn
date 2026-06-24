fd1f2a62864a75604936bda506a5f9a5c065b8d2
Lmyasdf <1901187412@qq.com>
2026-06-24 16:10:19 +0800

    docx： 丰富代码注释

--- Algorithm/DP/背包问题/完全背包/完全背包模板.cpp
+++ Algorithm/DP/背包问题/完全背包/完全背包模板.cpp
@@ -1,3 +1,5 @@
+//完全背包问题https://ac.nowcoder.com/acm/problem/226514
+//空间优化直接删除第一维就行，j一定要从小到大遍历
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -8,13 +10,18 @@ int main(){
     int n,m;
     cin>>n>>m;
     for(int i=1;i<=n;i++)cin>>v[i]>>w[i];
+    
+    //第一问 容量内的最大价值
     for(int i=1;i<=n;i++){
         for(int j=0;j<=m;j++){
+            //状态转移方程
             if(j>=v[i])f[i][j] = max(f[i-1][j],f[i][j-v[i]]+w[i]);
             else f[i][j] = f[i-1][j];
         }
     }
     cout<<f[n][m]<<endl;
+    
+    //第二问 恰好装满最大价值
     memset(f,-0x3f,sizeof(f));
     f[0][0] = 0;
     for(int i=1;i<=n;i++){
